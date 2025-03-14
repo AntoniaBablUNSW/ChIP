6f5f7314d61a077b1bdad80f59bf9b5a909ffb20
Antonia Babl <z5628505@ad.unsw.edu.au>
2025-03-14 15:38:26 +1100

    250314

--- Peakcallv2.sh
+++ Peakcallv2.sh
@@ -22,7 +22,7 @@ log2() {
 mkdir -p "$1/../Peaks"
 
 # Log basic infos
-log "Starting script Peakcall"
+log "Starting script Peakcallv2"
 log2 "User: $(whoami)"
 log2 "Working Directory: $(pwd)"
 log2 "System Info: $(uname -a)"
@@ -30,27 +30,47 @@ log2 "Active Conda Environment: $(conda info --envs | grep '*' | awk '{print $1}
 log2 "Conda Environment Details: $(conda list)"
 
 #LIST of Histone mods
-
 HisMods=("H3" "H4" "K27me3" "K27ac" "K4me3" "k4me3" "k27ac")
 
+# Define prefix to q-value mapping
+declare -A prefix_qvalue
+prefix_qvalue=( 
+    ["TAL1"]="0.05"
+    ["RUNX1"]="0.01"
+    ["GATA1"]="0.025"
+    ["KLF1"]="0.05"
+    ["BCL11A"]="0.01"
+    # Add more prefix to q-value mappings as needed
+)
+
+# Default q-value if prefix not found in mapping
+DEFAULT_QVALUE="0.05"
 
 for f in $1/../aligned/*.sorted.rmdup.bam; do
-    
-    
     
     basename="${f##*/}"  # Remove path
     basename="${basename%%.*}"  # Remove '.' suffix
 
- # Set variables
+    # Extract the prefix (first part before underscore)
+    prefix=$(echo "$basename" | cut -d'_' -f1)
+    
+    # Set q-value based on prefix
+    if [[ -n "${prefix_qvalue[$prefix]}" ]]; then
+        qvalue="${prefix_qvalue[$prefix]}"
+        log "Using q-value of $qvalue for prefix $prefix"
+    else
+        qvalue="$DEFAULT_QVALUE"
+        log "Using default q-value of $qvalue for prefix $prefix"
+    fi
+
+    # Set variables
     CHIP_BAM="$1/../aligned/$basename.sorted.rmdup.bam"
     INPUT_BAM="$2"
     OUTPUT_DIR="$1/../Peaks"
     GENOME_SIZE="hs"  # Use "hs" for human, "mm" for mouse, or specify the number of base pairs
     NAME="$basename"
 
-
-
- # Check if basename starts with any histone modification
+    # Check if basename starts with any histone modification
     is_histone_mod=0
     for mod in "${HisMods[@]}"; do
         if [[ "$basename" == "$mod"* ]]; then
@@ -61,63 +81,52 @@ for f in $1/../aligned/*.sorted.rmdup.bam; do
 
     if [[ $is_histone_mod -eq 1 ]]; then #Check if Histone_mod Was set to 1 
 
-    log "Running MACS2 for broad peak calling on $basename..."
+        log "Running MACS2 for broad peak calling on $basename with q-value $qvalue..."
         # Run MACS2 for broad peak calling
-    macs2 callpeak \
-        -t "$CHIP_BAM" \
-        -c "$INPUT_BAM" \
-        -f BAMPE \
-        -g "$GENOME_SIZE" \
-        -n "$NAME" \
-        --outdir "$OUTPUT_DIR" \
-        -q 0.05 \
-        --broad \
-        -B \
-        --keep-dup auto
+        macs2 callpeak \
+            -t "$CHIP_BAM" \
+            -c "$INPUT_BAM" \
+            -f BAMPE \
+            -g "$GENOME_SIZE" \
+            -n "$NAME" \
+            --outdir "$OUTPUT_DIR" \
+            -q "$qvalue" \
+            --broad \
+            -B \
+            --keep-dup auto
 
         if [ -f "$OUTPUT_DIR/${NAME}_peaks.broadPeak" ]; then
-            
-         log " Broad peak calling successful for $NAME"
-         
+            log " Broad peak calling successful for $NAME"
         else
             log " "
             echo "Error: Peak calling failed for $NAME."
             log " "
-            fi
-
-
-
-
+        fi
     
     else
- 
-    log "Running MACS2 for narrow peak calling on $basename..."
+        log "Running MACS2 for narrow peak calling on $basename with q-value $qvalue..."
 
-    # Run MACS2 for narrow peak calling
-    macs2 callpeak \
-        -t "$CHIP_BAM" \
-        -c "$INPUT_BAM" \
-        -f BAMPE \
-        -g "$GENOME_SIZE" \
-        -n "$NAME" \
-        --outdir "$OUTPUT_DIR" \
-        -q 0.05 \
-        -B \
-        --keep-dup auto\
-        --call-summits
+        # Run MACS2 for narrow peak calling
+        macs2 callpeak \
+            -t "$CHIP_BAM" \
+            -c "$INPUT_BAM" \
+            -f BAMPE \
+            -g "$GENOME_SIZE" \
+            -n "$NAME" \
+            --outdir "$OUTPUT_DIR" \
+            -q "$qvalue" \
+            -B \
+            --keep-dup auto \
+            --call-summits
 
-    
         if [ -f "$OUTPUT_DIR/${NAME}_peaks.narrowPeak" ]; then
-            
-         log "Narrow peak calling successful for $NAME"
-         
+            log "Narrow peak calling successful for $NAME"
         else
             log " "
             echo "Error: Peak calling failed for $NAME."
             log " "
-            fi
-
+        fi
     fi
-
 done
+
 log "End of Peak calling script"
